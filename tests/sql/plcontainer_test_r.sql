372c97b66455bb18b1ebf4c1eaa6fbbf6e51d3fa
Dave Cramer <davecramer@gmail.com>
2016-06-16 12:08:33 -0400

    Fixed R memory leak, strres needed PROTECT() temporarily disabled nested spi tests

--- tests/sql/plcontainer_test_r.sql
+++ tests/sql/plcontainer_test_r.sql
@@ -67,7 +67,7 @@ select rdimarr('{{1,1,0},{1,0,0}}'::bool[]);
 
 select rtimestamparr($${'2012-01-02 12:34:56.789012','2012-01-03 12:34:56.789012'}$$::timestamp[]);
 
-select rlog100_shared();
+--select rlog100_shared();
 select rpg_spi_exec('select 1');
 
 select rlogging();
@@ -91,8 +91,8 @@ select runargs3(123, 'foo', 'bar');
 select runargs4(1,null,null,1);
 
 select rnested_call_three('foo');
-select rnested_call_two('foobar');
-select rnested_call_one('foo1');
+--select rnested_call_two('foobar');
+--select rnested_call_one('foo1');
 select rtestudt1( ('t', 1, 2, 3, 4, 5, 6, 'foobar')::test_type );
 select rtestudt2( (
         array['t','f','t']::bool[],
