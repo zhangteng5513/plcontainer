8af279e0d788f1d4ff1cb4d602d567e06b91ac8f
Alexey Grishchenko <agrishchenko@pivotal.io>
2016-06-20 13:52:48 +0100

    First attempt to build gpdb4 in concourse

---
+++ concourse/scripts/gpdb4_build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir /usr/local/greenplum-db
+export BLD_ARCH=rhel5_x86_64
+cd gpdb4_src
+CFLAGS="-Wno-error=unused-but-set-variable" ./configure --prefix=/usr/local/greenplum-db --enable-depend --enable-debug --with-python --with-libxml || exit 1
+make prefix=/usr/local/greenplum-db || exit 1
+make install prefix=/usr/local/greenplum-db || exit 1
+cd ..
+tar -zcvf gpdb4_build/bin_gpdb4_centos7.tar.gz /usr/local/greenplum-db
