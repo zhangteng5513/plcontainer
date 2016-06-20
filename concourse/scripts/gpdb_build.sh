a55f0ca2f64505c0038143424e9942e3a17ca823
Alexey Grishchenko <agrishchenko@pivotal.io>
2016-06-20 10:56:40 +0100

    First attempt to add CI - building GPDB sources and collecting binary package

---
+++ concourse/scripts/gpdb_build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir /usr/local/greenplum-db
+export BLD_ARCH=rhel5_x86_64
+cd gpdb_src
+configure --prefix=/usr/local/greenplum-db --enable-depend --enable-debug --with-python --with-libxml || exit 1
+make || exit 1
+make install || exit 1
+cd ..
+tar -zcvf gpdb_build/bin_gpdb_centos7.tar.gz /usr/local/greenplum-db
