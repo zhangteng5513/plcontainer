f96f3dd8e4077b0eaa1fea5dcb40e43b8755ecee
Alexey Grishchenko <agrishchenko@pivotal.io>
2016-06-20 11:48:03 +0100

    Fixing the issue with "configure" in GPDB build

--- concourse/scripts/gpdb_build.sh
+++ concourse/scripts/gpdb_build.sh
@@ -3,7 +3,7 @@
 mkdir /usr/local/greenplum-db
 export BLD_ARCH=rhel5_x86_64
 cd gpdb_src
-configure --prefix=/usr/local/greenplum-db --enable-depend --enable-debug --with-python --with-libxml || exit 1
+./configure --prefix=/usr/local/greenplum-db --enable-depend --enable-debug --with-python --with-libxml || exit 1
 make || exit 1
 make install || exit 1
 cd ..
