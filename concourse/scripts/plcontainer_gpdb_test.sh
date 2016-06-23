4b6f919ab307daaf385cb231a808499c8e598f4e
Alexey Grishchenko <agrishchenko@pivotal.io>
2016-06-23 09:05:24 +0100

    Adding Docker logic to the testing script, PL/Container installation and starting the installcheck

--- concourse/scripts/plcontainer_gpdb_test.sh
+++ concourse/scripts/plcontainer_gpdb_test.sh
@@ -2,10 +2,15 @@
 
 set -x
 
+WORKDIR=`pwd`
+GPDBTAR=$1
+PLCGPPKG=$2
+OUTPUT=$3
+
 # Put GPDB binaries in place to get pg_config
-cp $1/$1.tar.gz /usr/local
+cp $GPDBTAR/$GPDBTAR.tar.gz /usr/local
 pushd /usr/local
-tar zxvf $1.tar.gz
+tar zxvf $GPDBTAR.tar.gz
 popd
 source /usr/local/greenplum-db/greenplum_path.sh || exit 1
 
@@ -16,21 +21,22 @@ setup_gpadmin_user
 setup_sshd
 
 # GPDB Installation
-cp plcontainer_src/concourse/scripts/gpdb_install.sh /tmp
-chmod 777 /tmp/gpdb_install.sh
+cp plcontainer_src/concourse/scripts/*.sh /tmp
+chmod 777 /tmp/*.sh
 runuser gpadmin -c "source /usr/local/greenplum-db/greenplum_path.sh && bash /tmp/gpdb_install.sh /data" || exit 1
-runuser gpadmin -c "source ~/.bashrc && gppkg -i $2/plcontainer-concourse.gppkg" || exit 1
 
 # Preparing for Docker and starting it
-#source plcontainer_src/concourse/scripts/docker_scripts.sh
-#start_docker || exit 1
-#echo 'agrishchenko@pivotal.io\n\n' | docker login -u agrishchenko -p MyDockerPassword9283 || exit 1
+source plcontainer_src/concourse/scripts/docker_scripts.sh
+start_docker || exit 1
+echo 'agrishchenko@pivotal.io\n\n' | docker login -u agrishchenko -p MyDockerPassword9283 || exit 1
 
 # Pulling new images from Docker Hub
-#docker pull pivotaldata/plcontainer_python:devel || exit 1
-#docker pull pivotaldata/plcontainer_r:devel || exit 1
-#docker pull pivotaldata/plcontainer_r_shared:devel || exit 1
-#docker pull pivotaldata/plcontainer_python_shared:devel || exit 1
-#docker pull pivotaldata/plcontainer_anaconda:devel || exit 1
+docker pull pivotaldata/plcontainer_python:devel || exit 1
+docker pull pivotaldata/plcontainer_r:devel || exit 1
+docker pull pivotaldata/plcontainer_r_shared:devel || exit 1
+docker pull pivotaldata/plcontainer_python_shared:devel || exit 1
+docker pull pivotaldata/plcontainer_anaconda:devel || exit 1
 
-#stop_docker || exit 1
+runuser gpadmin -c "bash /tmp/plcontainer_install_test.sh $WORKDIR $PLCGPPKG" || exit 1
+
+stop_docker || exit 1
---
+++ concourse/scripts/plcontainer_install_test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source ~/.bashrc
+
+set -x
+
+WORKDIR=$1
+PLCGPPKG=$2
+
+cd $WORKDIR
+
+# Install PL/Container package
+gppkg -i $PLCGPPKG/plcontainer-concourse.gppkg || exit 1
+cp plcontainer_src/management/config/plcontainer_configuration-ci.xml $GPHOME/share/postgresql/plcontainer/plcontainer_configuration.xml
+plcontainer-config --reset
+
+cd plcontainer_src
+make installcheck
