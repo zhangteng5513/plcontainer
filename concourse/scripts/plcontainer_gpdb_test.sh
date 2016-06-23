a38634999c9ee3ebdd2acb1f4befb06862ca1a49
Alexey Grishchenko <agrishchenko@pivotal.io>
2016-06-23 12:00:27 +0100

    Finalizing the testing scripts

--- concourse/scripts/plcontainer_gpdb_test.sh
+++ concourse/scripts/plcontainer_gpdb_test.sh
@@ -6,6 +6,7 @@ WORKDIR=`pwd`
 GPDBTAR=$1
 PLCGPPKG=$2
 OUTPUT=$3
+TMPDIR=/tmp/localplccopy
 
 # Put GPDB binaries in place to get pg_config
 cp $GPDBTAR/$GPDBTAR.tar.gz /usr/local
@@ -37,6 +38,27 @@ docker pull pivotaldata/plcontainer_r_shared:devel || exit 1
 docker pull pivotaldata/plcontainer_python_shared:devel || exit 1
 docker pull pivotaldata/plcontainer_anaconda:devel || exit 1
 
-runuser gpadmin -c "bash /tmp/plcontainer_install_test.sh $WORKDIR $PLCGPPKG" || exit 1
+runuser gpadmin -c "bash /tmp/plcontainer_install_test.sh $WORKDIR $PLCGPPKG $TMPDIR"
+RETCODE=$?
+
+if [ $RETCODE -ne 0 ]; then
+    echo "PL/Container test failed"
+    echo "====================================================================="
+    echo "========================= REGRESSION DIFFS =========================="
+    echo "====================================================================="
+    cat $TMPDIR/tests/regression.out
+    cat $TMPDIR/tests/regression.diffs
+    echo "====================================================================="
+    echo "============================== RESULTS =============================="
+    echo "====================================================================="
+    cat $TMPDIR/tests/results/plcontainer_test_python.out
+    cat $TMPDIR/tests/results/plcontainer_test_anaconda.out
+    cat $TMPDIR/tests/results/plcontainer_test_r.out
+else
+    echo "PL/Container test succeeded"
+    cp $PLCGPPKG/plcontainer-concourse.gppkg $OUTPUT/plcontainer-release.gppkg
+fi
 
 stop_docker || exit 1
+
+exit $RETCODE
--- concourse/scripts/plcontainer_install_test.sh
+++ concourse/scripts/plcontainer_install_test.sh
@@ -6,6 +6,7 @@ set -x
 
 WORKDIR=$1
 PLCGPPKG=$2
+TMPDIR=$3
 
 cd $WORKDIR
 
@@ -14,14 +15,7 @@ gppkg -i $PLCGPPKG/plcontainer-concourse.gppkg || exit 1
 cp plcontainer_src/management/config/plcontainer_configuration-ci.xml $GPHOME/share/postgresql/plcontainer/plcontainer_configuration.xml
 plcontainer-config --reset
 
-rm -rf /tmp/localplccopy
-cp -r plcontainer_src /tmp/localplccopy
-cd /tmp/localplccopy/tests
-make tests
-RETCODE=$?
-echo '======================================================================'
-psql -d pl_regression -c 'set client_min_messages=DEBUG1; select pylog100();'
-echo '======================================================================'
-docker run -d pivotaldata/plcontainer_python:devel ./client
-
-exit $RETCODE
+rm -rf $TMPDIR
+cp -r plcontainer_src $TMPDIR
+cd $TMPDIR/tests
+make tests || exit 1
