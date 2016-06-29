e663863d605c93902913624c8ba1696be6c333bc
Alexey Grishchenko <agrishchenko@pivotal.io>
2016-06-29 13:25:35 +0100

    Separate configuration for CI build is no longer needed

--- concourse/scripts/plcontainer_install_test.sh
+++ concourse/scripts/plcontainer_install_test.sh
@@ -13,7 +13,6 @@ cd $WORKDIR
 
 # Install PL/Container package
 gppkg -i $PLCGPPKG/plcontainer-concourse.gppkg || exit 1
-cp plcontainer_src/management/config/plcontainer_configuration-ci.xml $GPHOME/share/postgresql/plcontainer/plcontainer_configuration.xml
 plcontainer-config --reset
 
 rm -rf $TMPDIR
