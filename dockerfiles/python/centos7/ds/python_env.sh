0d0a0af2200dce56fc6fa3c55f256794c673e0c6
Hubert Zhang <hubertzhang@apache.org>
2017-08-31 16:43:56 +0800

    Add custom openssl in plcontainer docker image
    Signed-off-by: Haozhou Wang <hawang@pivotal.io>

--- dockerfiles/python/centos7/ds/python_env.sh
+++ dockerfiles/python/centos7/ds/python_env.sh
@@ -3,7 +3,7 @@ PYTHONHOME="/opt/python/rhel6_x86_64/python-2.7.12"
 PYTHONPATH=/opt/python/rhel6_x86_64/python-2.7.12/lib/python
 PYTHONPATH=/opt/ds/lib/python2.7/site-packages:$PYTHONPATH
 PATH=$PYTHONHOME/bin:$PATH
-LD_LIBRARY_PATH=$PYTHONHOME/lib:$LD_LIBRARY_PATH
+LD_LIBRARY_PATH=$PYTHONHOME/lib:/usr/local/lib:$LD_LIBRARY_PATH
 export PATH
 export LD_LIBRARY_PATH
 export PYTHONPATH
