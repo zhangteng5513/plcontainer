2f9c0527bbcdc601bba6060821e3c3f71ae4a627
Hubert Zhang <hubertzhang@apache.org>
2017-09-07 11:21:36 +0800

    Add option to specify image name explicitly for old verion docker

--- concourse/scripts/run_plcontainer_perf.sh
+++ concourse/scripts/run_plcontainer_perf.sh
@@ -18,7 +18,7 @@ ssh mdw "bash -c \" \
 set -eox pipefail; \
 export MASTER_DATA_DIRECTORY=/data/gpdata/master/gpseg-1; \
 source /usr/local/greenplum-db-devel/greenplum_path.sh; \
-plcontainer install -n plc_python_shared -i /usr/local/greenplum-db-devel/share/postgresql/plcontainer/plcontainer-devel-images.tar.gz; \
+plcontainer install -n plc_python_shared -i /usr/local/greenplum-db-devel/share/postgresql/plcontainer/plcontainer-devel-images.tar.gz -c pivotaldata/plcontainer_python_shared:devel; \
 gpstop -arf
 psql -d postgres -f /usr/local/greenplum-db-devel/share/postgresql/plcontainer/plcontainer_install.sql; \
 psql -d postgres -f plcontainer_src/concourse/scripts/function_setup.sql; \
