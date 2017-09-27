890323cd2962ce21f703982bb1c87afb4109d450
Paul Guo <paulguo@gmail.com>
2017-09-27 17:33:13 +0800

    No "yum update" in docker installation script on rhel6.

--- concourse/scripts/docker_install.sh
+++ concourse/scripts/docker_install.sh
@@ -7,7 +7,6 @@ install_docker() {
     case "$platform" in
       centos6)
         ssh centos@$node "sudo bash -c \"sudo rpm -iUvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm; \
- 	  sudo yum update -y; \
  	  sudo yum -y install docker-io; \
  	  sudo service docker start; \
  	  sudo groupadd docker; \
