84e16d90d1c5b28d49edfbe869ed9ecb1aa6a9eb
Paul Guo <paulguo@gmail.com>
2017-10-26 22:28:39 -0500

    Refactor the fault injection cases a bit. (#118)
    1) Reset fault injection before running fault injection cases.
    2) Split the cases since we encounter a weird psql issue. (Will address them later).
    3) Other small changes.

--- tests/sql/plcontainer_faultinject_python.sql
+++ tests/sql/plcontainer_faultinject_python.sql
@@ -11,6 +11,13 @@ CREATE TABLE tbl(i int);
 
 INSERT INTO tbl SELECT * FROM generate_series(1, 10);
 
+-- reset the injection points
+SELECT gp_inject_fault('plcontainer_before_container_started', 'reset', 2);
+SELECT gp_inject_fault('plcontainer_before_container_connected', 'reset', 2);
+SELECT gp_inject_fault('plcontainer_after_send_request', 'reset', 2);
+SELECT gp_inject_fault('plcontainer_after_recv_request', 'reset', 2);
+SELECT gp_inject_fault('plcontainer_before_udf_finish', 'reset', 2);
+
 -- start_ignore
 -- QE crash after start a container 
 SELECT gp_inject_fault('plcontainer_before_container_started', 'fatal', 2);
@@ -71,31 +78,6 @@ SELECT gp_inject_fault('plcontainer_after_send_request', 'reset', 2);
 SELECT gp_inject_fault('plcontainer_after_recv_request', 'reset', 2);
 SELECT gp_inject_fault('plcontainer_before_udf_finish', 'reset', 2);
 
--- After QE log(error, ...), related docker containers should be deleted.
--- start_ignore
-SELECT gp_inject_fault('plcontainer_before_container_started', 'error', 1);
-SELECT pyint(0);
-SELECT pg_sleep(5);
--- end_ignore
-
-\! docker ps -a | wc -l
-\! ps -ef | grep -v grep | grep "plcontainer cleaner" | wc -l
-SELECT pyint(0);
-
--- start_ignore
-SELECT gp_inject_fault('plcontainer_after_send_request', 'error', 1);
-SELECT pyint(0);
-SELECT pg_sleep(5);
--- end_ignore
-
-\! docker ps -a | wc -l
-\! ps -ef | grep -v grep | grep "plcontainer cleaner" | wc -l
-SELECT pyint(0);
--- Detect for the process name change (from "plcontainer cleaner" to other).
--- In such case, above cases will still succeed as unexpected.
-\! docker ps -a | wc -l
-\! ps -ef | grep -v grep | grep "plcontainer cleaner" | wc -l
-
--- reset the injection points
-SELECT gp_inject_fault('plcontainer_before_container_started', 'reset', 1);
-SELECT gp_inject_fault('plcontainer_after_send_request', 'reset', 1);
+DROP TABLE tbl;
+DROP FUNCTION pyint(i int);
+DROP EXTENSION gp_inject_fault;
