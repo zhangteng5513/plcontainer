19857fb60b487689d055a2262c4e862cb644f50b
Paul Guo <paulguo@gmail.com>
2017-11-27 15:05:09 +0800

    Fix pipeline failure.

--- concourse/scripts/run_plcontainer_tests.sh
+++ concourse/scripts/run_plcontainer_tests.sh
@@ -20,7 +20,6 @@ export MASTER_DATA_DIRECTORY=/data/gpdata/master/gpseg-1; \
 source /usr/local/greenplum-db-devel/greenplum_path.sh; \
 plcontainer install -n plc_python_shared -i /usr/local/greenplum-db-devel/share/postgresql/plcontainer/plcontainer-python-images.tar.gz -c pivotaldata/plcontainer_python_shared:devel -l python; \
 plcontainer install -n plc_r_shared -i /usr/local/greenplum-db-devel/share/postgresql/plcontainer/plcontainer-r-images.tar.gz -c pivotaldata/plcontainer_r_shared:devel -l r; \
-plcontainer configure -f plcontainer_src/tests/plcontainer_configuration_network.xml -y; \
 gpstop -arf ; \
 psql -d postgres -f /usr/local/greenplum-db-devel/share/postgresql/plcontainer/plcontainer_install.sql; \
 pushd plcontainer_src/tests; \
