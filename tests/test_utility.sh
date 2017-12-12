04296b0099be690092eb5f36bfa8bc635b815c64
Haozhou Wang <hawang@pivotal.io>
2017-12-12 18:24:47 +0800

    Refactor plcontainer runtime-list command

--- tests/test_utility.sh
+++ tests/test_utility.sh
@@ -21,9 +21,9 @@ echo "Test image-list: negative cases"
 plcontainer image-list >/tmp/tmp.image-list.out # We just care about whether there is stderr.
 plcontainer image-list not_exist
 
-echo "Prepare a blank runtime configuration file and test runtime-list"
+echo "Prepare a blank runtime configuration file and test runtime-backup"
 #Backup the runtime configurations.
-plcontainer runtime-list >test_backup_cfg_file
+plcontainer runtime-backup -f test_backup_cfg_file
 cat <<EOF >tmp_cfg_file
 <?xml version="1.0" ?>
 <configuration>
@@ -33,24 +33,24 @@ plcontainer runtime-restore -f tmp_cfg_file \
 	| sed -e 's/.*ERROR]://' -e 's/.*INFO]://' -e 's/.*CRITICAL]://' \
 	| grep -v 'Distributing to'
 # We do not want $GPHOME to be in exp output.
-plcontainer runtime-list |  sed -e "s|${GPHOME}|GPHOME|"
+plcontainer runtime-backup |  sed -e "s|${GPHOME}|GPHOME|"
 rm -f tmp_cfg_file
 
-######################## test runtime-add/delte/list ###############################
+######################## test runtime-add/delte/show/backup ###############################
 echo "Test runtime-add: nagative cases"
 plcontainer runtime-add -r nonexist_runtime
 plcontainer runtime-add -i nonexist_image
 plcontainer runtime-add -r nonexist_runtime -i nonexist_image
 plcontainer runtime-add -r nonexist_runtime -i nonexist_image -l java
 
-echo "Test runtime-add, runtime-list, and runtime-delete"
+echo "Test runtime-add, runtime-backup, and runtime-delete"
 plcontainer runtime-add -r runtime1 -i image1 -l python \
 	| sed -e 's/.*ERROR]://' -e 's/.*INFO]://' -e 's/.*CRITICAL]://' \
 	| grep -v 'Distributing to'
-plcontainer runtime-list |  sed -e "s|${GPHOME}|GPHOME|"
+plcontainer runtime-backup |  sed -e "s|${GPHOME}|GPHOME|"
 plcontainer runtime-delete -r runtime1 | sed -e 's/.*ERROR]://' -e 's/.*INFO]://' -e 's/.*CRITICAL]://' \
 	| grep -v 'Distributing to'
-plcontainer runtime-list |  sed -e "s|${GPHOME}|GPHOME|"
+plcontainer runtime-backup |  sed -e "s|${GPHOME}|GPHOME|"
 plcontainer runtime-delete -r runtime1 | sed -e 's/.*ERROR]://' -e 's/.*INFO]://' -e 's/.*CRITICAL]://' \
 	| grep -v 'Distributing to'
 
@@ -75,17 +75,18 @@ plcontainer runtime-add -r runtime1 -i image1 -l python -v /host_dir1/shared1:/c
 	| sed -e 's/.*ERROR]://' -e 's/.*INFO]://' -e 's/.*CRITICAL]://' \
 	| grep -v 'Distributing to'
 plcontainer runtime-add -r runtime2 -i image2 -l r -v /host_dir2/shared1:/container_dir2/shared1:rw \
-		-v /host_dir2/shared2:/container_dir2/shared2:ro -s memory_mb=512 -s user_network=yes \
+		-v /host_dir2/shared2:/container_dir2/shared2:ro -s memory_mb=512 -s use_network=yes \
 	| sed -e 's/.*ERROR]://' -e 's/.*INFO]://' -e 's/.*CRITICAL]://' \
 	| grep -v 'Distributing to'
-plcontainer runtime-list |  sed -e "s|${GPHOME}|GPHOME|"
+plcontainer runtime-show |  sed -e "s|${GPHOME}|GPHOME|"
+plcontainer runtime-show -r runtime1 |  sed -e "s|${GPHOME}|GPHOME|"
 plcontainer runtime-delete -r runtime1 \
 	| sed -e 's/.*ERROR]://' -e 's/.*INFO]://' -e 's/.*CRITICAL]://' \
 	| grep -v 'Distributing to'
 plcontainer runtime-delete -r runtime2 \
 	| sed -e 's/.*ERROR]://' -e 's/.*INFO]://' -e 's/.*CRITICAL]://' \
 	| grep -v 'Distributing to'
-plcontainer runtime-list |  sed -e "s|${GPHOME}|GPHOME|"
+plcontainer runtime-show |  sed -e "s|${GPHOME}|GPHOME|"
 
 echo "Test runtime-replace: negative cases"
 plcontainer runtime-replace -r runtime3
@@ -93,16 +94,16 @@ plcontainer runtime-replace -r runtime3 -i image3
 plcontainer runtime-replace -r runtime3 -i image3 -l java
 echo "Test runtime-replace: add a new one"
 plcontainer runtime-replace -r runtime3 -i image2 -l r -v /host_dir3/shared1:/container_dir3/shared1:rw \
-	-v /host_dir3/shared2:/container_dir3/shared2:ro -s memory_mb=512 -s user_network=yes \
+	-v /host_dir3/shared2:/container_dir3/shared2:ro -s memory_mb=512 -s use_network=yes \
 	| sed -e 's/.*ERROR]://' -e 's/.*INFO]://' -e 's/.*CRITICAL]://' \
 	| grep -v 'Distributing to'
-plcontainer runtime-list |  sed -e "s|${GPHOME}|GPHOME|"
+plcontainer runtime-backup |  sed -e "s|${GPHOME}|GPHOME|"
 echo "Test runtime-replace: replace"
 plcontainer runtime-replace -r runtime3 -i image2 -l r -v /host_dir3/shared3:/container_dir3/shared3:rw \
-	-s user_network=no \
+	-s use_network=no \
 	| sed -e 's/.*ERROR]://' -e 's/.*INFO]://' -e 's/.*CRITICAL]://' \
 	| grep -v 'Distributing to'
-plcontainer runtime-list |  sed -e "s|${GPHOME}|GPHOME|"
+plcontainer runtime-backup |  sed -e "s|${GPHOME}|GPHOME|"
 
 echo "test runtime-backup"
 plcontainer runtime-backup >/tmp/runtime-backup.stdout
