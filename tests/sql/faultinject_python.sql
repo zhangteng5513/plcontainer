22b3b069459ecef604efdbe0a8aaee986a4ce41e
Paul Guo <paulguo@gmail.com>
2017-12-18 14:24:32 +0800

    Fix a long known issue that faultinject case could abort if the sql file is not small.
    It seems that when the sql file is larger than ~4K, the case could ~ abort at the 4K offset.
    The reason seems to be that some spawn external commands (i.e. \! in sql file)  (ssh?) could
    swallow stdin which is expected for psql.
    
    A simple fix is to add "</dev/null" for all external commands in our sql file.
    
    Another fix seems to be that that pg_regress should use "-f $sql_file" instead of "< $sql_file".
        snprintf(psql_cmd, sizeof(psql_cmd),
    	-            "%s " SYSTEMQUOTE "\"%s%spsql\" -X -a -q -d \"%s\" < \"%s\" > \"%s\" 2>&1" SYSTEMQUOTE,
    	+            "%s " SYSTEMQUOTE "\"%s%spsql\" -X -a -q -d \"%s\" -f \"%s\" > \"%s\" 2>&1" SYSTEMQUOTE,
    
    No we fix in our case at first so that we could tolerate any code of pg_regress.
    
    Of course, faultinject2_python is added back to faultinject_python and thus is removed.

--- tests/sql/faultinject_python.sql
+++ tests/sql/faultinject_python.sql
@@ -27,8 +27,8 @@ SELECT pyint(i) from tbl;
 SELECT pg_sleep(5);
 -- end_ignore
 
-\! ssh `psql -d ${PL_TESTDB} -c 'select address from gp_segment_configuration where dbid=2' -t -A` docker ps -a | wc -l
-\! ssh `psql -d ${PL_TESTDB} -c 'select address from gp_segment_configuration where dbid=2' -t -A` ps -ef | grep -v grep | grep "plcontainer cleaner" | wc -l
+\! ssh `psql -d ${PL_TESTDB} -c 'select address from gp_segment_configuration where dbid=2' -t -A` docker ps -a </dev/null | wc -l
+\! ssh `psql -d ${PL_TESTDB} -c 'select address from gp_segment_configuration where dbid=2' -t -A` ps -ef </dev/null | grep -v grep | grep "plcontainer cleaner" | wc -l
 SELECT sum(pyint(i)) from tbl;
 
 -- start_ignore
@@ -40,8 +40,8 @@ SELECT pyint(i) from tbl;
 SELECT pg_sleep(5);
 -- end_ignore
 
-\! ssh `psql -d ${PL_TESTDB} -c 'select address from gp_segment_configuration where dbid=2' -t -A` docker ps -a | wc -l
-\! ssh `psql -d ${PL_TESTDB} -c 'select address from gp_segment_configuration where dbid=2' -t -A` ps -ef | grep -v grep | grep "plcontainer cleaner" | wc -l
+\! ssh `psql -d ${PL_TESTDB} -c 'select address from gp_segment_configuration where dbid=2' -t -A` docker ps -a </dev/null | wc -l
+\! ssh `psql -d ${PL_TESTDB} -c 'select address from gp_segment_configuration where dbid=2' -t -A` ps -ef </dev/null | grep -v grep | grep "plcontainer cleaner" | wc -l
 SELECT sum(pyint(i)) from tbl;
 
 -- start_ignore
@@ -50,8 +50,8 @@ SELECT pyint(i) from tbl;
 SELECT pg_sleep(5);
 -- end_ignore
 
-\! ssh `psql -d ${PL_TESTDB} -c 'select address from gp_segment_configuration where dbid=2' -t -A` docker ps -a | wc -l
-\! ssh `psql -d ${PL_TESTDB} -c 'select address from gp_segment_configuration where dbid=2' -t -A` ps -ef | grep -v grep | grep "plcontainer cleaner" | wc -l
+\! ssh `psql -d ${PL_TESTDB} -c 'select address from gp_segment_configuration where dbid=2' -t -A` docker ps -a </dev/null | wc -l
+\! ssh `psql -d ${PL_TESTDB} -c 'select address from gp_segment_configuration where dbid=2' -t -A` ps -ef </dev/null | grep -v grep | grep "plcontainer cleaner" | wc -l
 SELECT sum(pyint(i)) from tbl;
 
 -- start_ignore
@@ -60,8 +60,8 @@ SELECT pyint(i) from tbl;
 SELECT pg_sleep(5);
 -- end_ignore
 
-\! ssh `psql -d ${PL_TESTDB} -c 'select address from gp_segment_configuration where dbid=2' -t -A` docker ps -a | wc -l
-\! ssh `psql -d ${PL_TESTDB} -c 'select address from gp_segment_configuration where dbid=2' -t -A` ps -ef | grep -v grep | grep "plcontainer cleaner" | wc -l
+\! ssh `psql -d ${PL_TESTDB} -c 'select address from gp_segment_configuration where dbid=2' -t -A` docker ps -a </dev/null | wc -l
+\! ssh `psql -d ${PL_TESTDB} -c 'select address from gp_segment_configuration where dbid=2' -t -A` ps -ef </dev/null | grep -v grep | grep "plcontainer cleaner" | wc -l
 SELECT sum(pyint(i)) from tbl;
 
 -- start_ignore
@@ -70,8 +70,8 @@ SELECT pyint(i) from tbl;
 SELECT pg_sleep(5);
 -- end_ignore
 
-\! ssh `psql -d ${PL_TESTDB} -c 'select address from gp_segment_configuration where dbid=2' -t -A` docker ps -a | wc -l
-\! ssh `psql -d ${PL_TESTDB} -c 'select address from gp_segment_configuration where dbid=2' -t -A` ps -ef | grep -v grep | grep "plcontainer cleaner" | wc -l
+\! ssh `psql -d ${PL_TESTDB} -c 'select address from gp_segment_configuration where dbid=2' -t -A` docker ps -a </dev/null | wc -l
+\! ssh `psql -d ${PL_TESTDB} -c 'select address from gp_segment_configuration where dbid=2' -t -A` ps -ef </dev/null | grep -v grep | grep "plcontainer cleaner" | wc -l
 
 -- reset the injection points
 SELECT gp_inject_fault('plcontainer_before_container_started', 'reset', 2);
@@ -81,5 +81,41 @@ SELECT gp_inject_fault('plcontainer_after_recv_request', 'reset', 2);
 SELECT gp_inject_fault('plcontainer_before_udf_finish', 'reset', 2);
 
 DROP TABLE tbl;
+
+-- reset the injection points
+SELECT gp_inject_fault('plcontainer_before_container_started', 'reset', 1);
+SELECT gp_inject_fault('plcontainer_after_send_request', 'reset', 1);
+
+-- After QE log(error, ...), related docker containers should be deleted.
+-- Test on entrydb.
+-- start_ignore
+show optimizer;
+SELECT gp_inject_fault('plcontainer_before_container_started', 'error', 1);
+SELECT pyint(0);
+SELECT pg_sleep(5);
+-- end_ignore
+
+\! docker ps -a </dev/null | wc -l
+\! ps -ef </dev/null | grep -v grep | grep "plcontainer cleaner" | wc -l
+SELECT pyint(1);
+
+-- start_ignore
+SELECT gp_inject_fault('plcontainer_after_send_request', 'error', 1);
+SELECT pyint(2);
+SELECT pg_sleep(5);
+-- end_ignore
+
+\! docker ps -a </dev/null | wc -l
+\! ps -ef </dev/null | grep -v grep | grep "plcontainer cleaner" | wc -l
+SELECT pyint(3);
+-- Detect for the process name change (from "plcontainer cleaner" to other).
+-- In such case, above cases will still succeed as unexpected.
+\! docker ps -a </dev/null | wc -l
+\! ps -ef </dev/null | grep -v grep | grep "plcontainer cleaner" | wc -l
+
+-- reset the injection points
+SELECT gp_inject_fault('plcontainer_before_container_started', 'reset', 1);
+SELECT gp_inject_fault('plcontainer_after_send_request', 'reset', 1);
+
 DROP FUNCTION pyint(i int);
 DROP EXTENSION gp_inject_fault;
