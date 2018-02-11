55c3d3c920dbe095309684559236e6df0a00fc97
Haozhou Wang <hawang@pivotal.io>
2018-02-11 16:29:08 +0800

    Add drop extension tests
    1. Add drop extension tests in regression test
    2. fix an uninstall script issue

--- management/sql/plcontainer--unpackaged--1.0.0.sql
+++ management/sql/plcontainer--unpackaged--1.0.0.sql
@@ -5,6 +5,9 @@ DROP VIEW IF EXISTS plcontainer_show_config;
 
 DROP FUNCTION IF EXISTS plcontainer_refresh_local_config(verbose bool);
 DROP FUNCTION IF EXISTS plcontainer_show_local_config();
+DROP FUNCTION IF EXISTS plcontainer_containers_summary();
+
+DROP TYPE IF EXISTS container_summary_type;
 
 DROP LANGUAGE IF EXISTS plcontainer CASCADE;
 DROP FUNCTION IF EXISTS plcontainer_call_handler();
